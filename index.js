83e67ab36220ac75c2f09c067c91dbd015f2f9c7
themuppetman <157922635+themuppetman@users.noreply.github.com>
2024-04-22 14:46:03 -0700

    Rendering Issues

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const {Circle, Triangle, Square} = require('./lib/shapes.js');
 
 // Generate the SVG based on user input
-function generateSVG(text, textColor, shape, shapeColor) {
-    // Generate SVG string based on user input
-    const svg = `<svg width="" height="">
-                    <text x="" y="" fill="${textColor}">${text}</text>
-                    <${shape} fill="${shapeColor}" cx="" cy="" r=""/>
-                </svg>`;
-    return svg;
+function generateSVG(data) {
+    let svgContent = `<svg width="300" height="200" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">`;
+
+    // Add text element
+    svgContent += `<text x="50" y="100" fill="${data.textColor}">${data.text}</text>`;
+
+    // Add shape element
+    switch (data.shape) {
+        case 'Circle':
+            svgContent += `<circle cx="100" cy="100" r="50" fill="${data.shapeColor}"/>`;
+            break;
+        case 'Triangle':
+            svgContent += `<polygon points="100,50 50,150 150,150" fill="${data.shapeColor}"/>`;
+            break;
+        case 'Square':
+            svgContent += `<rect x="50" y="50" width="100" height="100" fill="${data.shapeColor}"/>`;
+            break;
+    }
+
+    svgContent += `</svg>`;
+
+    return svgContent;
 }
 
 function saveSVG(svg) {
@@ -16,7 +32,7 @@ function saveSVG(svg) {
 }
 
 function promptUser() {
-    inquirer.prompt([
+   return inquirer.prompt([
         {
             type: 'input',
             name: 'text',
@@ -28,22 +44,27 @@ function promptUser() {
             message: 'Enter text color (keyword or hexadecimal number):'
         },
         {
-            type: 'list',
-            name: 'shape',
+            type: 'select',
+            name: 'Shape',
             message: 'Choose a shape:',
-            choices: ['circle', 'triangle', 'square']
+            choices: ['Circle', 'Triangle', 'Square']
         },
         {
             type: 'input',
             name: 'shapeColor',
             message: 'Enter shape color (keyword or hexadecimal number):'
         }
-    ]).then(answers => {
+    ])
+    .then(answers => {
         const svg = generateSVG(answers.text, answers.textColor, answers.shape, answers.shapeColor);
         saveSVG(svg);
         console.log('Generated logo.svg');
     });
+    
+
 }
+
+
 
 // Call the promptUser function to start the application
 promptUser();
